2d9c2f3d5a19c2c328082fe71a7454667ba59c5f
Yasir <112541705+sirosiyasir@users.noreply.github.com>
2022-10-14 20:21:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+document.querySelector(".firstQuestion").addEventListener("click", function() {
+  var x = document.querySelector(".firstAnswer");
+  var down = document.querySelector(".firstDown");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    down.style.transform = "rotate(180deg)";
+  } else {
+    x.style.display = "none";
+    down.style.transform = "rotate(0deg)";
+  }
+})
+
+document.querySelector(".secondQuestion").addEventListener("click", function() {
+  var x = document.querySelector(".secondAnswer");
+  var down = document.querySelector(".secondDown");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    down.style.transform = "rotate(180deg)";
+  } else {
+    x.style.display = "none";
+    down.style.transform = "rotate(0deg)";
+  }
+})
+
+document.querySelector(".thirdQuestion").addEventListener("click", function() {
+  var x = document.querySelector(".thirdAnswer");
+  var down = document.querySelector(".thirdDown");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    down.style.transform = "rotate(180deg)";
+  } else {
+    x.style.display = "none";
+    down.style.transform = "rotate(0deg)";
+  }
+})
+
+document.querySelector(".fourthQuestion").addEventListener("click", function() {
+  var x = document.querySelector(".fourthAnswer");
+  var down = document.querySelector(".fourthDown");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    down.style.transform = "rotate(180deg)";
+  } else {
+    x.style.display = "none";
+    down.style.transform = "rotate(0deg)";
+  }
+})
+
+document.querySelector(".fifthQuestion").addEventListener("click", function() {
+  var x = document.querySelector(".fifthAnswer");
+  var down = document.querySelector(".fifthDown");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    down.style.transform = "rotate(180deg)";
+  } else {
+    x.style.display = "none";
+    down.style.transform = "rotate(0deg)";
+  }
+})
